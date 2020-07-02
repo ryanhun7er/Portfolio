e29fae6a4ccfa891c547dcdbbc7e22d837bc0630
Ryan Hunter <ryanhwilkinson@icloud.com>
2020-07-02 15:03:02 -0400

    add footer

---
+++ portfolio/src/components/Layouts/Footer.js
@@ -0,0 +1,13 @@
+
+import React from "react";
+
+
+function Footer() {
+  return (
+    <footer className="fixed-bottom footer">
+      <span className="footer">Ryan Hunter &copy; 2020</span>
+    </footer>
+  );
+}
+
+export default Footer;
