4cfd5cb42ca4a78ec39ebc7a71fbbfb6806b23ee
Ryan Hunter <ryanhwilkinson@icloud.com>
2020-07-02 16:51:24 -0400

    update about js

--- portfolio/src/App.js
+++ portfolio/src/App.js
@@ -1,35 +1,30 @@
-import React, {Component} from 'react';
-import { HashRouter as Router, Route} from "react-router-dom";
-import Wrapper from "./components/Wrapper";
-import Navbar from "./components/Layouts/Navbar";
-import Projects from "./pages/Projects";
-import About from "./pages/About";
+import React, { Component } from 'react';
+import { HashRouter as Router, Route } from 'react-router-dom';
+import Wrapper from './components/Wrapper';
+import Navbar from './components/Layouts/Navbar';
+import Projects from './pages/Projects';
+import About from './pages/About';
 import Contact from './pages/Contact';
-import Footer from "./components/Layouts/Footer";
+import Footer from './components/Layouts/Footer';
 import './style.css';
 
-
-
 class App extends Component {
-  
-  render(){
-  return (
-   <Router>
-     <div>
-     <Navbar />
+	render() {
+		return (
+			<Router>
+				<div>
+					<Navbar />
 
-      <Wrapper>
-        <Route exact path="/About" component={About}/>
-        <Route exact path="/Projects" component={Projects}/>
-        <Route exact path="/Contact" component={Contact}/>
-      </Wrapper>
-      <Footer/>
-     </div>
-   </Router>
-    );
-
-
-  }
+					<Wrapper>
+						<Route exact path='/About' component={About} />
+						<Route exact path='/Projects' component={Projects} />
+						<Route exact path='/Contact' component={Contact} />
+					</Wrapper>
+					<Footer />
+				</div>
+			</Router>
+		);
+	}
 }
 
 export default App;
--- portfolio/src/components/Layouts/Footer.js
+++ portfolio/src/components/Layouts/Footer.js
@@ -1,13 +1,11 @@
-
-import React from "react";
-
+import React from 'react';
 
 function Footer() {
-  return (
-    <footer className="fixed-bottom footer">
-      <span className="footer">Ryan Hunter &copy; 2020</span>
-    </footer>
-  );
+	return (
+		<footer className='fixed-bottom footer'>
+			<span className='footer'>Ryan Hunter &copy; 2020</span>
+		</footer>
+	);
 }
 
 export default Footer;
