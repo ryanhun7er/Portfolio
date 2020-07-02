764d29f2722e261f7f5fcd3def7a7b9aedb68f65
Ryan Hunter <ryanhwilkinson@icloud.com>
2020-07-02 18:04:11 -0400

    updates

--- portfolio/src/App.js
+++ portfolio/src/App.js
@@ -2,11 +2,9 @@ import React, { Component } from 'react';
 import { HashRouter as Router, Route } from 'react-router-dom';
 import Wrapper from './components/Wrapper';
 import Navbar from './components/Layouts/Navbar';
-import Projects from './pages/Projects';
-import About from './pages/About';
-import Contact from './pages/Contact';
+import About from './pages/Page';
 import Footer from './components/Layouts/Footer';
-import './style.css';
+import './css/style.css';
 
 class App extends Component {
 	render() {
@@ -16,9 +14,7 @@ class App extends Component {
 					<Navbar />
 
 					<Wrapper>
-						<Route exact path='/About' component={About} />
-						<Route exact path='/Projects' component={Projects} />
-						<Route exact path='/Contact' component={Contact} />
+						<Route exact path='/' component={About} />
 					</Wrapper>
 					<Footer />
 				</div>
---
+++ portfolio/src/components/Layouts/Navbar.js
@@ -0,0 +1,92 @@
+import React from 'react';
+import { Link } from 'react-router-dom';
+import '../../css/style.css';
+
+// Depending on the current path, this component sets the "active" class on the appropriate navigation link item
+function Navbar() {
+	return (
+		<div id='nav'>
+			<nav className='navbar navbar-custom'>
+				<div className='container'>
+					<Link className='navbar-brand' to='/'>
+						R y a n W i l k i n s o n
+					</Link>
+					<div>
+						<ul className='nav navbar-nav'>
+							<li className='hidden'>
+								<a href='#page-top'></a>
+							</li>
+							<Link
+								to='/'
+								className={
+									window.location.pathname === '/' ||
+									window.location.pathname === '/'
+										? 'nav-link active'
+										: 'nav-link'
+								}>
+								Home
+							</Link>
+							<li className='page-scroll' href='#about'>
+								<Link
+									to='#about'
+									className={
+										window.location.pathname === '#about'
+											? 'nav-link active'
+											: 'nav-link'
+									}>
+									About
+								</Link>
+							</li>{' '}
+							<li className='page-scroll' href='#skills'>
+								<Link
+									to='#skills'
+									className={
+										window.location.pathname === '#skills'
+											? 'nav-link active'
+											: 'nav-link'
+									}>
+									Skills
+								</Link>
+							</li>{' '}
+							<li className='page-scroll' href='#portfolio'>
+								<Link
+									to='#portfolio'
+									className={
+										window.location.pathname === '#porfolio'
+											? 'nav-link active'
+											: 'nav-link'
+									}>
+									Portfolio
+								</Link>
+							</li>{' '}
+							<li className='page-scroll' href='#resume'>
+								<Link
+									to='#resume'
+									className={
+										window.location.pathname === '#resume'
+											? 'nav-link active'
+											: 'nav-link'
+									}>
+									Resume
+								</Link>
+							</li>{' '}
+							<li className='page-scroll' href='#contact'>
+								<Link
+									to='#contact'
+									className={
+										window.location.pathname === '#contact'
+											? 'nav-link active'
+											: 'nav-link'
+									}>
+									Contact
+								</Link>
+							</li>{' '}
+						</ul>
+					</div>
+				</div>
+			</nav>
+		</div>
+	);
+}
+
+export default Navbar;
